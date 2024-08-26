d27b58ed1302490d01fc83a061484ec33753b409
Aymar <dolatoundel@gmail.com>
2024-08-25 22:28:02 -0400

    Dealer profil done

---
+++ run_v1.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo " Going to team-b-school"
+cd team-b-school
+
+echo "Pulling latest code"
+#git fetch origin
+#git reset --hard origin/main
+
+echo "Building docker image"
+docker build -t registry.gitlab.com/monverdict/team-b-school .
+
+docker login -u Shadows97 -p glpat-1bJMW-7QSwRaXMJ8sQJL registry.gitlab.com
+
+echo "Pushing docker image"
+docker push registry.gitlab.com/monverdict/team-b-school
+
+echo "Going to root"
+cd ..
+
+
+kubectl create configmap laravel-config-map \
+    --from-env-file=team-b-school/.env \
+    --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Deploying docker image"
+su gitlab-runner -c "./deploy.sh"
+
+su gitlab-runner -c "kubectl rollout restart deployment.apps/laravel-app"
